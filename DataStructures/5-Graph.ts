99cbf4483de1606995a2bad2189a7af2f72a318b
Shubham Dhage <shubham.dhage@swiggy.in>
2022-02-08 10:03:55 +0530

    Graohs

--- DataStructures/5-Graph.ts
+++ DataStructures/5-Graph.ts
@@ -1,3 +1,17 @@
+// Graphs - The simplest way to define a graph would be anything that is a collection of nodes in which the nodes are connected by edges. Graphs are one of the most popular mathematical concepts that are used in computer science.
+
+// Popular examples of graph implementations would be any social media websites these days. Facebook uses friends as nodes and friendship as edges. Twitter, on the other hand, defines followers as nodes and following as an edge, and so on.
+
+// Types of Graphs:
+// 1. Simple Graphs
+// 2. Undirected Graphs
+// 3. Directed Graphs
+// 4. Cyclic Graphs
+// 5. Directed Acyclic Graphs
+// 6. Weighted Graphs
+
+// A bidirected graph is a graph in which each edge is given an independent orientation at each end.
+
 interface Users {
     id: number;
     name: string;
@@ -23,8 +37,95 @@ class Graph {
         });
     }
 
-    shortestPath(sourceUser, targetCompany) {
-        
+    shortestPath(sourceUser: Users, targetCompany: string) {
+        // final shortestPath
+        var shortestPath;
+
+        // for iterating along the breadth
+        var tail = 0;
+
+        // queue of users being visited
+        var queue: Users[] = [ sourceUser ];
+
+        // mark visited users
+        var visitedNodes: number[] = [];
+
+        // previous path to backtrack steps when shortestPath is found
+        var prevPath = {};
+
+        // request is same as response
+        if (sourceUser.company === targetCompany) {
+            return;
+        }
+
+        // mark source user as visited so next time we skip the processing
+        visitedNodes.push(sourceUser.id);
+
+        // loop queue until match is found
+        // Or until the end of queue i.e no match
+        while (!shortestPath && tail < queue.length) {
+
+            // take user breadth first
+            var user = queue[tail];
+
+            // take nodes forming edges with user
+            var friendsIds: number[] = this.edges[user.id];
+
+            // loop over each node
+            friendsIds.forEach(friendId => {
+                // result found in previous iteration, so we can stop
+                if (shortestPath) return;
+
+                // get all details of node
+                var friend = this.users.find(user => user.id === friendId);
+
+                // if visited already, nothing to recheck so return
+                if (visitedNodes.includes(friendId)) {
+                    return;
+                }
+
+                // mark as visited
+                visitedNodes.push(friendId);
+
+                // if company matched
+                if (friend.company === targetCompany) {
+
+                    // create result path with the matched node
+                    var path: Users[] = [friend];
+
+                    // keep backtracking until source user and add to path
+                    while (user.id !== sourceUser.id) {
+                        // add user to shortest path
+                        path.unshift(user);
+
+                        // prepare for next iteration
+                        user = prevPath[user.id];
+                    }
+
+                    // add source user to the path
+                    path.unshift(user);
+
+                    // format and return shortest path
+                    shortestPath = path.map(path => path.name).join(' -> ');
+                }
+
+                // break loop if shortestPath found
+                if (shortestPath) return;
+
+                // no match found at current user,
+                // add it to previous path to help backtracking later
+                prevPath[friend.id] = user;
+
+                // add to queue in the order of visit
+                // i.e. breadth wise for next iteration
+                queue.push(friend);
+            });
+
+            // increment counter
+            tail++;
+        }
+
+        return shortestPath || `No path between ${shortestPath.name} & ${targetCompany}`;
     }
 }
 
@@ -79,3 +180,4 @@ const users: Users[] = [
     },
 ];
 const graph = new Graph(users);
+console.log(graph.shortestPath(users[2], 'Samsung'));
